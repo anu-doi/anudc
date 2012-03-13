7369afd3b56bf69421dbb9efd5b9e049885883df
Genevieve Turner <genevieve.turner@anu.edu.au>
2012-03-13 13:18:47 +1100

    Update for database connections

---
+++ DataCommons/extras/sql/1_create_database.sql
@@ -0,0 +1,13 @@
+/**
+ * 1_create_database.sql
+ * 
+ * Australian National University Data Commons
+ * 
+ * Creates a database (note this must be run as a super user e.g. postgres)
+ * 
+ * Version	Date		Developer			Description
+ * 0.1		13/03/2012	Genevieve Turner	Initial build
+ */
+
+CREATE USER dcuser WITH PASSWORD 'dcpassword';
+CREATE DATABASE datacommonsdb OWNER dcuser;
---
+++ DataCommons/extras/sql/drop_tables.sql
@@ -0,0 +1,15 @@
+/**
+ * drop_tables.sql
+ * 
+ * Australian National University Data Commons
+ * 
+ * Deletes all the tables we have added.
+ * This is for development purposes only.
+ * 
+ * Version	Date		Developer			Description
+ * 0.1		13/03/2012	Genevieve Turner	Initial build
+ */
+
+DROP TABLE user_group;
+DROP TABLE access_group;
+DROP TABLE access_user;
