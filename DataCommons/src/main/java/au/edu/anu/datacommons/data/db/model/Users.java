0858cf4dc22a71ade3887e92504818f964ac2f13
Genevieve Turner <genevieve.turner@anu.edu.au>
2012-05-16 15:57:26 +1000

    Added missing file

---
+++ DataCommons/src/main/java/au/edu/anu/datacommons/data/db/model/Users.java
@@ -0,0 +1,54 @@
+package au.edu.anu.datacommons.data.db.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class Users {
+	private Long id;
+	private String username;
+	private String password;
+	private Boolean enabled;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name="username")
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Column(name="password")
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Column(name="enabled")
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+}
