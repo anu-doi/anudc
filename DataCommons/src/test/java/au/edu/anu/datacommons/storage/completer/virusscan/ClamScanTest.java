d6dd27b8105068b896d15ddeb5bc6f6c3724861d
Rahul Khanna <rahul.khanna@anu.edu.au>
2013-06-05 11:12:48 +1000

    Added a unit test to scan a file.

--- DataCommons/src/test/java/au/edu/anu/datacommons/storage/completer/virusscan/ClamScanTest.java
+++ DataCommons/src/test/java/au/edu/anu/datacommons/storage/completer/virusscan/ClamScanTest.java
@@ -2,18 +2,28 @@ package au.edu.anu.datacommons.storage.completer.virusscan;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import au.edu.anu.datacommons.storage.completer.virusscan.ClamScan;
+import au.edu.anu.datacommons.storage.info.ScanResult;
+import au.edu.anu.datacommons.test.util.TestUtil;
 
 public class ClamScanTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ClamScanTest.class);
+	
+	@Rule
+	public TemporaryFolder tempDir = new TemporaryFolder();
 	
 	private ClamScan clamScan;
 	
@@ -27,6 +37,7 @@ public class ClamScanTest {
 
 	@Before
 	public void setUp() throws Exception {
+		clamScan = new ClamScan("dc7-dev2.anu.edu.au", 3310);
 	}
 
 	@After
@@ -35,7 +46,14 @@ public class ClamScanTest {
 
 	@Test
 	public void testPing() {
-		clamScan = new ClamScan("dc7-dev2.anu.edu.au", 3310);
 		assertTrue(clamScan.ping());
+	}
+	
+	@Test
+	public void testScan() throws IOException {
+		File file = tempDir.newFile();
+		TestUtil.fillRandomData(file, 10);
+		ScanResult result = clamScan.scan(new FileInputStream(file));
+		LOGGER.trace(result.getResult());
 	}
 }
