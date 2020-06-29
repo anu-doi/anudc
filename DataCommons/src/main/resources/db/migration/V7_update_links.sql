ec4b8f722270e53ca326977c7b7a8cabe8474706
Navnit Maudgil <u1096965@anu.edu.au>
2020-06-29 16:30:00 +1000

    Updated a href to use the css class. Fixed typos and converted text to link

--- DataCommons/src/main/resources/db/migration/V7_update_links.sql
+++ DataCommons/src/main/resources/db/migration/V7_update_links.sql
@@ -1,10 +1,12 @@
 --fixing typo in link licence to license
 UPDATE template_attribute 
-SET tooltip = replace(tooltip, 'http://www.opendatacommons.org/licences/pddl/1.0/', '<a target="_blank" rel="noopener noreferrer" href="https://www.opendatacommons.org/licenses/pddl/1.0/">https://www.opendatacommons.org/licenses/pddl/1.0/</a>');
+SET tooltip = replace(tooltip, 'http://www.opendatacommons.org/licences/pddl/1.0/', '<a target="_blank" rel="noopener noreferrer" href="https://www.opendatacommons.org/licenses/pddl/1.0/" class="text-link">https://www.opendatacommons.org/licenses/pddl/1.0/</a>');
+
 
 --replacing ausgoal link
 UPDATE template_attribute 
-SET tooltip = replace(tooltip, 'http://www.ausgoal.gov.au/the-ausgoal-licence-suite', '<a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/">https://creativecommons.org/licenses/</a>');
+SET tooltip = replace(tooltip, 'http://www.ausgoal.gov.au/the-ausgoal-licence-suite', '<a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/licenses/" class="text-link">https://creativecommons.org/licenses/</a>');
+
 
 --correcting typo mateiral to material
 UPDATE template_attribute 
@@ -12,7 +14,7 @@ SET tooltip = replace(tooltip, 'mateiral', 'material');
 
 --fixing data management link
 UPDATE template_attribute 
-SET tooltip = replace(tooltip, 'http://libguides.anu.edu.au/datamanagement/', '<a target="_blank" rel="noopener noreferrer" href="https://libguides.anu.edu.au/research-data-management">https://libguides.anu.edu.au/research-data-management</a>');
+SET tooltip = replace(tooltip, 'http://libguides.anu.edu.au/datamanagement/', '<a target="_blank" rel="noopener noreferrer" href="https://libguides.anu.edu.au/research-data-management" class="text-link">https://libguides.anu.edu.au/research-data-management</a>');
 
 --making links open in new tab
 UPDATE template_attribute 
@@ -26,3 +28,11 @@ SET tooltip = replace(tooltip, '<a href="https://www.abs.gov.au/Ausstats/abs@.ns
 UPDATE template_attribute 
 SET tooltip = replace(tooltip, '<a href="http://www.loc.gov/catdir/cpso/lcco/" class="text-link">', 
 			       '<a target="_blank" rel="noopener noreferrer" href="http://www.loc.gov/catdir/cpso/lcco/" class="text-link">');
+			       
+--updating text to link
+UPDATE template_attribute 
+SET tooltip = replace(tooltip, 'http://www.gnu.org/copyleft/gpl.html', '<a target="_blank" rel="noopener noreferrer" href="http://www.gnu.org/copyleft/gpl.html" class="text-link">http://www.gnu.org/copyleft/gpl.html</a>');
+
+--fixing typo
+UPDATE template_attribute 
+SET tooltip = replace(tooltip, '<strong>General Public Licenc</strong>', '<strong>General Public Licence</strong>');
