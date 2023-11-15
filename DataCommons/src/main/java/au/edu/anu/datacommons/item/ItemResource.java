7c4e33dc6892080a5fcc9cb2d3d91d3b44173e56
u5125986 <u5125986@ITS-081603.UDS.anu.edu.au>
2023-11-15 15:48:13 +1100

    INC0842202 - Fix item view

--- DataCommons/src/main/java/au/edu/anu/datacommons/item/ItemResource.java
+++ DataCommons/src/main/java/au/edu/anu/datacommons/item/ItemResource.java
@@ -21,6 +21,8 @@
 
 package au.edu.anu.datacommons.item;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -28,14 +30,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.xml.bind.JAXBException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 
+import au.edu.anu.datacommons.comparator.TemplateAttributeSortByDisplay;
 import au.edu.anu.datacommons.data.db.model.FedoraObject;
+import au.edu.anu.datacommons.data.db.model.Template;
+import au.edu.anu.datacommons.freemarker.GroupOptions;
+import au.edu.anu.datacommons.freemarker.SecurityCheck;
+import au.edu.anu.datacommons.freemarker.SelectOptions;
 import au.edu.anu.datacommons.security.service.FedoraObjectService;
+import au.edu.anu.datacommons.storage.info.RecordDataSummary;
+import au.edu.anu.datacommons.xml.data.Data;
+import au.edu.anu.datacommons.xml.sparql.Result;
 
 import com.sun.jersey.api.view.Viewable;
+import com.yourmediashelf.fedora.client.FedoraClientException;
 
 /**
  * ItemResource
@@ -58,6 +74,8 @@ import com.sun.jersey.api.view.Viewable;
 @Component
 @Scope("request")
 public class ItemResource {
+	static final Logger LOGGER = LoggerFactory.getLogger(ItemResource.class);
+	
 	@Resource(name = "fedoraObjectServiceImpl")
 	private FedoraObjectService fedoraObjectService;
 	
@@ -77,10 +95,63 @@ public class ItemResource {
 	@GET
 	@Path("{item}")
 	public Response getItem(@PathParam("item") String item) {
+		LOGGER.info("In /item getItem", item);
 		FedoraObject fedoraObject = fedoraObjectService.getItemByPid(item);
-		Map<String, Object> values = fedoraObjectService.getViewPage(fedoraObject, "def:display", null);
-
-		Viewable viewable = new Viewable((String) values.remove("topage"), values);
-		return Response.ok(viewable).build();
+		if (fedoraObject == null) {
+			return Response.status(Status.NOT_FOUND).entity("Item not found").build();
+		}
+		try {
+			fedoraObjectService.verifyActive(fedoraObject);
+			Template template = fedoraObjectService.getTemplateByTemplateId(fedoraObject.getTmplt_id());
+			
+			template.getTemplateAttributes().sort(new TemplateAttributeSortByDisplay());
+			
+			Data editData = null;
+			Data publishData = null;
+			try {
+				editData = fedoraObjectService.getEditData(fedoraObject);
+			}
+			catch (Exception e) {
+				LOGGER.error("Exception retrieving edit data", e);
+			}
+			try {
+				publishData = fedoraObjectService.getPublishData(fedoraObject);
+			}
+			catch (Exception e) {
+				LOGGER.error("Exception retrieving publish data: {}", e.getMessage());
+			}
+			if (editData == null && publishData == null) {
+				throw new AccessDeniedException("You do not have access to this resource");
+			}
+			Map<String, Object> values = new HashMap<String, Object>();
+			if (publishData == null) {
+				values.put("data", editData);
+				
+			}
+			else {
+				values.put("data", publishData);
+				Data differences = fedoraObjectService.getDataDifferences(template, editData, publishData);
+				values.put("differenceData", differences);
+				
+			}
+			RecordDataSummary rdi = fedoraObjectService.getRecordDataSummary(fedoraObject);
+			List<Result> links = fedoraObjectService.getLinks(fedoraObject);
+			
+			values.put("tmplt", template);
+			values.put("item", fedoraObject);
+			values.put("rdi", rdi);
+			values.put("links", links);
+			values.put("options", new SelectOptions());
+			values.put("groups", new GroupOptions());
+			values.put("security",new SecurityCheck(fedoraObject));
+//			values.put("errormessage", errorMessage);
+			
+			Viewable viewable = new Viewable("/display/display.ftl", values);
+			return Response.ok(viewable).build();
+		}
+		catch (JAXBException | FedoraClientException e) {
+			LOGGER.error("Exception retrieing template", e);
+		}
+		return Response.serverError().build();
 	}
 }
