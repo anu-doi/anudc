0eb83c88152d1744e5e89e4b03bcb5d2d2dea5e6
u5125986 <u5125986@ITS-081603.UDS.anu.edu.au>
2025-10-27 15:58:17 +1100

    Fix incorrect select code for the type

--- DataCommons/src/main/resources/db/migration/V11_add_related_info_option.sql
+++ DataCommons/src/main/resources/db/migration/V11_add_related_info_option.sql
@@ -22,7 +22,7 @@ insert into template_attribute (id, template_id, name, field_type_id, label, too
 (243,1,'externalParty',5,'Related xxternal people and organisations','If appropriate, include identifiers for related external people and organisations ie. ROR',false,false,null,null,2, 47, 47, null, null);
 
 insert into template_attribute_column (template_attribute_id, name, label, field_type_id, select_code, column_order) values
-	(243,'partyIdType', 'Identifier type', 3, 'identifierType', 1)
+	(243,'partyIdType', 'Identifier type', 3, 'partyIdType', 1)
 	,(243,'partyIdValue', 'Identifier value', 1, null, 2)
 	,(243,'partyTitle', 'Title/Name', 1, null, 3)
 	,(243,'partyRelationType', 'Relationship type', 3, 'collectionPartyType', 4)
